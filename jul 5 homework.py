a6d0a6de86418fa720907134b0ae9914cdb19b9e
sanayk-ai <sanayk.acg@gmail.com>
2026-07-16 23:00:15 +1200

    homework

---
+++ jul 5 homework.py
@@ -0,0 +1,12 @@
+denzel = "June 6th 2012"
+jiho = "August 13th 2012"
+ollie = "September 24th 2012"
+leo = "November 19th 2012"
+stella = "February 18th 2013"
+
+print("My friends birthdays are:\n")
+print("Denzel's:", denzel)
+print("\nJiho's:", jiho)
+print("\nOllie's:", ollie)
+print("\nLeo's:", leo)
+print("\nStella's:", stella)
